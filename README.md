2ccfa5c88874dc60fc48b5e809e9d6fac9384d93
JoeMcCormick <59099839+JoeMcCormick-Serenity@users.noreply.github.com>
2022-06-16 12:56:49 -0500

    Bump version: 1.2.0 → 1.2.1

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # apyi
-## Version 1.2.0
+## Version 1.2.1
 
 Creates model objects from openapi specifications that can be used/customized for operation execution. 
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name ='apyi',
-    version = '1.2.0',
+    version = '1.2.1',
     author = 'Joel McCormick',
     author_email = 'joe.lp.mccormick@gmail.com',
     description = 'TODO: write a description',
